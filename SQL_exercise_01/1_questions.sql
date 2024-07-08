ee59ddc6fc9a0733696117d9178dd1310d1a3b05
Bobby Shu <101524606+Bobbyshu@users.noreply.github.com>
2024-07-08 17:44:27 -0400

    format sql

--- SQL_exercise_01/1_questions.sql
+++ SQL_exercise_01/1_questions.sql
@@ -4,16 +4,35 @@ select name from products;
 -- 1.2 Select the names and the prices of all the products in the store.
 select name, price from products;
 -- 1.3 Select the name of the products with a price less than or equal to $200.
-select name from products where price <= 200;
+select name
+from products 
+where price <= 200;
 -- 1.4 Select all the products with a price between $60 and $120.
-select * from products where price >= 60 and price <= 200;
+select * 
+from products 
+where price >= 60 and price <= 200;
 -- 1.5 Select the name and price in cents (i.e., the price must be multiplied by 100).
-select name, price 
+select name, 
+(price * 100) AS cents 
+from products; 
 -- 1.6 Compute the average price of all the products.
+select AVG(price) from products;
 -- 1.7 Compute the average price of all products with manufacturer code equal to 2.
+select AVG(price) 
+from products
+where manufacturer = 2;
 -- 1.8 Compute the number of products with a price larger than or equal to $180.
+select COUNT(*)
+from products
+where price >= 180;
 -- 1.9 Select the name and price of all products with a price larger than or equal to $180, and sort first by price (in descending order), and then by name (in ascending order).
+select name, price 
+from products where price >= 180 
+order by price desc, name asc;
 -- 1.10 Select all the data from the products, including all the data for each product's manufacturer.
+select * from products 
+inner join manufacturers 
+where products.manufacturer=manufacturers.code;
 -- 1.11 Select the product name, price, and manufacturer name of all the products.
 -- 1.12 Select the average price of each manufacturer's products, showing only the manufacturer's code.
 -- 1.13 Select the average price of each manufacturer's products, showing the manufacturer's name.
